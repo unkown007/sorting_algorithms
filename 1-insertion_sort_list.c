82e7716d24ae66b65cce6a6162ed4b67b0663329
unkown007 <victormatusse00@gmail.com>
2023-04-24 20:47:47 +0200

    added file

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,48 +1,58 @@
 #include "sort.h"
 
 /**
+ * swap_list - swaps the position of two nodes in a doubly linked list
+ * @list: head of the list
+ * @prev: previous node
+ * @node: current node
+ * @next: next node
+ */
+void swap_list(listint_t **list, listint_t *prev,
+		listint_t *node, listint_t *next)
+{
+	while (prev && node->n < prev->n)
+	{
+		if (next)
+			next->prev = prev;
+		node->next = prev;
+		node->prev = prev->prev;
+		if (prev->prev)
+			(prev->prev)->next = node;
+		prev->prev = node;
+		prev->next = next;
+		next = prev;
+		prev = node->prev;
+		if ((*list)->prev)
+			*list = (*list)->prev;
+		print_list(*list);
+	}
+}
+
+/**
  * insertion_sort_list - sorts a doubly linked list of integers in
  * ascending order
  * @list: doubly listint_t linked list
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *k, *node, *i, *j;
+	listint_t *node, *prev, *next;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
-	k = (*list);
-	while (k)
+	if (list && *list && (*list)->next)
 	{
-		for (i = (*list)->next; i; i = i->next)
+		node = (*list)->next;
+		prev = node->prev;
+		next = node->next;
+
+		while (node)
 		{
-			for (j = *list; j != i && j->next; j = j->next)
+			if (node->n < prev->n)
+				swap_list(list, prev, node, next);
+			node = next;
+			if (node)
 			{
-				if (j->n > j->next->n)
-				{
-					node = j->next;
-					if (i == node)
-						i = j;
-					node->prev->next = node->next;
-					if (node->next != NULL)
-						node->next->prev = node->prev;
-					node->next = j;
-					if (j == *list)
-					{
-						node->prev = NULL;
-						*list = node;
-					}
-					else
-					{
-						j->prev->next = node;
-						node->prev = j->prev;
-					}
-					j->prev = node;
-					j = j->prev;
-					print_list(*list);
-				}
+				next = node->next;
+				prev = node->prev;
 			}
 		}
-		k = k->next->next;
 	}
 }
