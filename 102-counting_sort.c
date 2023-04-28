e9350d728665ac9d3261e0d1c35019a1669f5ced
unkown007 <victormatusse00@gmail.com>
2023-04-28 15:19:46 +0200

    added file

---
+++ 102-counting_sort.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+/**
+ * counting_sort - sorts an array of integers in asceding order
+ * @array: array of integers
+ * @size: size of the array
+ */
+void counting_sort(int *array, size_t size)
+{
+}
---
+++ 104-heap_sort.c
@@ -0,0 +1,54 @@
+#include "sort.h"
+
+/**
+ * heap - creates the heap in an array of integers
+ * @array: array of integers
+ * @beg: beginning of the array
+ * @end: last element of the array
+ * @size: size of the array
+ */
+void heap(int *array, int beg, int end, size_t size)
+{
+	int aux = array[beg];
+	int j = beg * 2 + 1;
+
+	while (j <= end)
+	{
+		if (j < end)
+		{
+			if (array[j] < array[j + 1])
+				j += 1;
+		}
+		if (aux < array[j])
+		{
+			array[beg] = array[j];
+			beg = j;
+			j = 2 * beg + 1;
+			print_array(array, size);
+		}
+		else
+			j = end + 1;
+	}
+	array[beg] = aux;
+}
+
+/**
+ * heap_sort - sorts an array of integers in ascending order
+ * @array: heap sort
+ * @size: array size
+ */
+void heap_sort(int *array, size_t size)
+{
+	int i, aux;
+
+	for (i = (size - 1) / 2; i >= 0; i--)
+		heap(array, i, size - 1, size);
+	for (i = size - 1; i >= 1; i--)
+	{
+		aux = array[0];
+		array[0] = array[i];
+		array[i] = aux;
+		heap(array, 0, i - 1, size);
+		print_array(array, size);
+	}
+}
